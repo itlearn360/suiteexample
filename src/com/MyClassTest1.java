6c40682a026bd4717c932dc40bf1800d7f6a36f6
itlearn360 <training@itlearn360.com>
2018-08-30 14:01:50 +0530

    Update TestClass

--- src/com/MyClassTest1.java
+++ src/com/MyClassTest1.java
@@ -6,6 +6,7 @@ public class MyClassTest1 extends SeleniumAbstractTest{
 	  @Test
 	  public void myTestMethod1() {
 	    System.out.println("myTestMethod1 Update update123");
+	    System.out.println("myTestMethod1");
 	  }
 	  @Test
 	  public void myTestMethod2() {
