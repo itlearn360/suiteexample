d711342cea52e645560ac0a81058ed2340e5bac5
itlearn360 <training@itlearn360.com>
2018-08-30 13:35:43 +0530

    Saving the file to repository

---
+++ src/com/MyClassTest1.java
@@ -0,0 +1,15 @@
+package com;
+
+import org.testng.annotations.Test;
+
+public class MyClassTest1 extends SeleniumAbstractTest{
+	  @Test
+	  public void myTestMethod1() {
+	    System.out.println("myTestMethod1 Update update again one");
+	  }
+	  @Test
+	  public void myTestMethod2() {
+	    System.out.println("myTestMethod2");
+	  }
+
+}
