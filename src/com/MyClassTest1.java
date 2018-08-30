660fb2fa0e63d2e82a304a7e555a2f8011868d42
itlearn360 <40198951+itlearn360@users.noreply.github.com>
2018-08-30 14:01:29 +0530

    Update MyClassTest1.java

--- src/com/MyClassTest1.java
+++ src/com/MyClassTest1.java
@@ -5,7 +5,7 @@ import org.testng.annotations.Test;
 public class MyClassTest1 extends SeleniumAbstractTest{
 	  @Test
 	  public void myTestMethod1() {
-	    System.out.println("myTestMethod1 Update update123");
+	    System.out.println("myTestMethod1 Update update1234");
 	  }
 	  @Test
 	  public void myTestMethod2() {
